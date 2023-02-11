fc35a30e4a74641aedeb7fb2f2af8bbb8ebb0b40
Sang Bui <xsang.bui@gmail.com>
2023-02-11 13:38:11 +0700

    fog: Evolution X bringup

--- evolution_fog.mk
+++ evolution_fog.mk
@@ -8,19 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
-# Alpha internal properties
-ALPHA_BUILD_TYPE := Official
-ALPHA_MAINTAINER := ⓓⓑⓛⓔⓝⓚ
-TARGET_ENABLE_BLUR := true
+# Additional stuff
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := evolution_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
